3c0a57e3da86d9f2c7989adafd12b9eacd427e65
fancq <1191071905@qq.com>
2019-08-15 13:36:13 +0800

    新增注释

--- src/main/java/com/tydic/zookeeper/ZooKeeperProSync.java
+++ src/main/java/com/tydic/zookeeper/ZooKeeperProSync.java
@@ -26,6 +26,7 @@ public class ZooKeeperProSync implements Watcher {
     @Override
     public void process(WatchedEvent watchedEvent) {
         if (watchedEvent.getState() == Event.KeeperState.SyncConnected) { // zk连接成功通知事件
+            System.out.println("成功连接ZK！");
             if (Event.EventType.None == watchedEvent.getType() && null == watchedEvent.getPath()) {
                 connectedSemaphore.countDown();
             } else if (watchedEvent.getType() == Event.EventType.NodeDataChanged) { // zk目录节点数据变化通知事件
@@ -39,6 +40,7 @@ public class ZooKeeperProSync implements Watcher {
     }
 
     public static void main(String[] args) throws IOException, InterruptedException, KeeperException {
+        System.out.println("开始连接ZK...");
         // zookeeper配置数据存放路径
         String path = "/username";
         // 连接zookeeper并且注册一个默认的监听器
@@ -47,6 +49,7 @@ public class ZooKeeperProSync implements Watcher {
         connectedSemaphore.await();
         // 获取path目录节点的配置数据，并注册默认的监听器
         System.out.println("获取【" + path + "】目录节点配置数据【" + new String(zk.getData(path, true, stat)) + "】");
+        System.out.println("输入任何内容按下回车键结束程序！");
         System.in.read(); // 阻塞，防止程序退出
         zk.close();
     }
